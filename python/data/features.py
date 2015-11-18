4e754a71ce025733d205200a101f3a37c791a98c
gfpastorek <gfpastorek@gmail.com>
2015-11-18 02:07:31 -0600

    added momentum feature

--- python/data/features.py
+++ python/data/features.py
@@ -1,21 +1,29 @@
 import unittest
 import pandas as pd
 import numpy as np
+import scipy as sp
 
 def generate_features():
     raise NotImplemented("TODO")
 
 
-def add_dema(data, halflives=[10, 40, 100]):
-
+def add_dema(data, halflives=[10, 40, 100], colname='dEMA'):
     for hl in halflives:
         ema = pd.ewma(data['price'], halflife=hl)
-        data['dEMA_{}'.format(hl)] = 0
-        data.ix[1:, 'dEMA_{}'.format(hl)] = pd.ewma(np.diff(ema), halflife=hl)
-        data['dEMA_std_{}'.format(hl)] = pd.rolling_std(data['dEMA_{}'.format(hl)], 2*hl)   # TODO, what window to use?
+        data['{}_{}'.format(colname, hl)] = 0
+        data.ix[1:, '{}_{}'.format(colname, hl)] = pd.ewma(np.diff(ema), halflife=hl)
+        data['{}_std_{}'.format(colname, hl)] = \
+            pd.rolling_std(data['{}_{}'.format(colname, hl)], 2*hl)   # TODO, what window to use?
 
 
-def add_ema(data, halflives=[10, 40, 100]):
-
+def add_ema(data, halflives=[10, 40, 100], colname='EMA'):
     for hl in halflives:
-        data['EMA_{}'.format(hl)] = pd.ewma(data['price'], halflife=hl)
+        data['{}_{}'.format(colname, hl)] = pd.ewma(data['price'], halflife=hl)
+
+
+def add_momentum(data, halflives=[10, 40, 100], colname='momentum'):
+    add_ema(data, halflives=halflives)
+    data[colname] = 0
+    for i in xrange(0, len(data)):
+        ranking = sp.stats.rankdata(data[['EMA_{}'.format(hl) for hl in halflives]].iloc[i])
+        data[colname].iloc[i] = sp.spatial.distance.hamming(ranking, range(1, len(halflives)+1))
