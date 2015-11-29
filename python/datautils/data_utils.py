544372cc15323eca9194a2dd8c64547ff592973c
gfpastorek <gfpastorek@gmail.com>
2015-11-28 21:02:14 -0600

    made download.py also get trades

--- python/datautils/data_utils.py
+++ python/datautils/data_utils.py
@@ -43,7 +43,10 @@ def _clean_quotes(data, start_hour=9, start_min=30, end_hour=15, end_min=30, bar
 
     data = filter_invalid_quotes(data)
     data = data.drop(['SYM_SUFFIX', 'NATBBO_IND'], 1)
-    print data.columns
+    try:
+        data = data.drop(['BIDEX', 'ASKEX'], 1)
+    except:
+        pass
     data.columns = ['DATE_TIME', 'SYM', 'BID_PRICE', 'BID_SIZE', 'ASK_PRICE', 'ASK_SIZE']
 
     data = data.set_index('DATE_TIME')
@@ -89,12 +92,7 @@ def get_quotes(ticker, year, month, day, bar_width='second'):
         root_dir = os.path.realpath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
     except NameError:
         root_dir = os.path.realpath(os.path.dirname(os.getcwd()))
-    fpatho = os.path.join(root_dir, 'data', filename, '{}.csv'.format(filename))
     fpath = os.path.join(root_dir, 'data', filename, '{}_quotes.csv'.format(filename))
-    try:
-        os.rename(fpatho, fpath)
-    except:
-        pass
     data = pd.read_csv(fpath, parse_dates=[['DATE', 'TIME_M']], date_parser=_convert_time)
     data = _clean_quotes(data, bar_width=bar_width)
     return data
@@ -139,7 +137,7 @@ def get_more_data(tickers, year, month, day, days=1, bar_width='second', max_cou
                 quotes, trades = get_data(ticker, y, m, d, bar_width=bar_width)
                 data.append((quotes, trades))
                 c += 1
-            except IOError:
+            except (IOError, ValueError):
                 continue
     return data
 
--- python/datautils/download.py
+++ python/datautils/download.py
@@ -8,15 +8,15 @@ from selenium.webdriver.support.ui import Select
 
 root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-if len(sys.argv) < 3:
-    print("usage: download.py YEAR COMPANY_CODES")
-    print("usage: download.py 2012 IBM MSFT DELL")
-    #sys.exit()
-    year = '2012'
-    tickers = ['xle']
-else:
-    year = sys.argv[1]
-    tickers = sys.argv[2:]
+#if len(sys.argv) < 3:
+##    print("usage: download.py YEAR COMPANY_CODES")
+#    print("usage: download.py 2012 IBM MSFT DELL")
+#    #sys.exit()
+
+years = [2012]
+tickers = ['xle']
+months = [2]
+
 
 # log in
 driver = webdriver.Firefox()
@@ -27,83 +27,168 @@ username_box.send_keys('xh2013')
 password_box.send_keys('Xh66916202')
 password_box.submit()
 
-# query
-for month in range(1, 2):
-    for day in range(monthrange(int(year), month+1)[1]):
-        print("downloading " + str(month+1) + '/' + str(day+1) + '/'+year)
-        driver.get('https://wrds-web.wharton.upenn.edu/wrds/ds/taq/cqm/index.cfm')
 
-        ticker = " ".join(tickers).strip()
-        company_codes = driver.find_element_by_id('code-lookup')
-        company_codes.send_keys(ticker)
+def download_quotes(ticker, year, month, day):
+    driver.get('https://wrds-web.wharton.upenn.edu/wrds/ds/taq/cqm/index.cfm')
+    company_codes = driver.find_element_by_id('code-lookup')
+    company_codes.send_keys(ticker)
 
-        # variables
-        driver.find_element_by_id('var_BID').click()
-        driver.find_element_by_id('var_BIDSIZ').click()
-        driver.find_element_by_id('var_ASK').click()
-        driver.find_element_by_id('var_ASKSIZ').click()
-        driver.find_element_by_id('var_NATBBO_IND').click()
-        driver.find_element_by_id('csv').click()
-        driver.find_element_by_id('none').click()
+    # variables
+    driver.find_element_by_id('var_BID').click()
+    driver.find_element_by_id('var_BIDSIZ').click()
+    driver.find_element_by_id('var_ASK').click()
+    driver.find_element_by_id('var_ASKSIZ').click()
+    driver.find_element_by_id('var_NATBBO_IND').click()
+    driver.find_element_by_id('csv').click()
+    driver.find_element_by_id('none').click()
 
-        # time
-        beg_m = Select(driver.find_element_by_id('beg_m'))
-        beg_d = Select(driver.find_element_by_id('beg_d'))
-        beg_yr = Select(driver.find_element_by_id('beg_yr'))
-        end_m = Select(driver.find_element_by_id('end_m'))
-        end_d = Select(driver.find_element_by_id('end_d'))
-        end_yr = Select(driver.find_element_by_id('end_yr'))
-        beg_m.select_by_index(month)
-        end_m.select_by_index(month)
-        beg_d.select_by_index(day)
-        end_d.select_by_index(day)
-        beg_yr.select_by_visible_text(year)
-        end_yr.select_by_visible_text(year)
+    # time
+    beg_m = Select(driver.find_element_by_id('beg_m'))
+    beg_d = Select(driver.find_element_by_id('beg_d'))
+    beg_yr = Select(driver.find_element_by_id('beg_yr'))
+    end_m = Select(driver.find_element_by_id('end_m'))
+    end_d = Select(driver.find_element_by_id('end_d'))
+    end_yr = Select(driver.find_element_by_id('end_yr'))
+    beg_m.select_by_index(month-1)
+    end_m.select_by_index(month-1)
+    beg_d.select_by_index(day-1)
+    end_d.select_by_index(day-1)
+    beg_yr.select_by_visible_text(str(year))
+    end_yr.select_by_visible_text(str(year))
+    end_hh = Select(driver.find_element_by_id('end_hh'))
+    end_hh.select_by_visible_text('24')
+    driver.find_element_by_id('form_submit').click()
 
-        end_hh = Select(driver.find_element_by_id('end_hh'))
-        end_hh.select_by_visible_text('24')
+    # switch focus to newly opened tab
+    time.sleep(5)
+    new_tab = driver.window_handles[-1]
+    driver.close()
+    driver.switch_to.window(new_tab)
 
-        driver.find_element_by_id('form_submit').click()
+    # wait for query to process
+    start_time = time.time()
+    result = None
 
-        # switch focus to newly opened tab
+    while ('No matches found.' not in driver.page_source
+           and int(time.time() - start_time) < 240
+           and result is None):
+
         time.sleep(5)
-        new_tab = driver.window_handles[-1]
-        driver.close()
-        driver.switch_to.window(new_tab)
+        try:
+            result = driver.find_element_by_partial_link_text('.csv')
+        except:
+            pass
 
-        # wait for query to process
-        start_time = time.time()
-        result = None
-        while ('No matches found.' not in driver.page_source
-                and int(time.time() - start_time) < 240
-                and result is None):
+    if result is None:
+        return
 
-            time.sleep(5)
-            try:
-                result = driver.find_element_by_partial_link_text('.csv')
-            except:
-                pass
+    all_cookies = driver.get_cookies()
+    cookies = {}
+    for s_cookie in all_cookies:
+        cookies[s_cookie["name"]] = s_cookie["value"]
 
-        # download results
-        filename = ticker.replace(" ", "_") + '_' + str(month+1).zfill(2) + '_' + str(day+1).zfill(2) + '_' + year[2:]
-        filename = filename + os.path.sep + filename + '_quotes.csv'
-        filename = os.path.join(root_dir, 'data', filename)
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-        with open(filename, 'wb') as handle:
-            if result is not None:
-                all_cookies = driver.get_cookies()
-                cookies = {}
-                for s_cookie in all_cookies:
-                    cookies[s_cookie["name"]] = s_cookie["value"]
+    url = result.get_attribute('href')
+    response = requests.get(url, cookies=cookies, stream=True, verify=False)
 
-                url = result.get_attribute('href')
-                response = requests.get(url, cookies=cookies, stream=True, verify=False)
+    if not response.ok:
+        print("Bad Response")
+        return
 
-                if not response.ok:
-                    print("Bad Response")
-                for block in response.iter_content(1024):
-                    handle.write(block)
+    # download results
+    filename = ticker.replace(" ", "_") + '_' + str(month).zfill(2) + '_' + str(day).zfill(2) + '_' + str(year)[2:]
+    filename = filename + os.path.sep + filename + '_quotes.csv'
+    filename = os.path.join(root_dir, 'data', filename)
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, 'wb') as handle:
+        for block in response.iter_content(1024):
+            handle.write(block)
+
+
+def download_trades(ticker, year, month, day):
+    driver.get('https://wrds-web.wharton.upenn.edu/wrds/ds/taq/ctm/index.cfm')
+    company_codes = driver.find_element_by_id('code-lookup')
+    company_codes.send_keys(ticker)
+
+    # variables
+    driver.find_element_by_id('var_SIZE').click()
+    driver.find_element_by_id('var_PRICE').click()
+    driver.find_element_by_id('csv').click()
+    driver.find_element_by_id('none').click()
+
+    # time
+    beg_m = Select(driver.find_element_by_id('beg_m'))
+    beg_d = Select(driver.find_element_by_id('beg_d'))
+    beg_yr = Select(driver.find_element_by_id('beg_yr'))
+    end_m = Select(driver.find_element_by_id('end_m'))
+    end_d = Select(driver.find_element_by_id('end_d'))
+    end_yr = Select(driver.find_element_by_id('end_yr'))
+    beg_m.select_by_index(month-1)
+    end_m.select_by_index(month-1)
+    beg_d.select_by_index(day-1)
+    end_d.select_by_index(day-1)
+    beg_yr.select_by_visible_text(str(year))
+    end_yr.select_by_visible_text(str(year))
+    end_hh = Select(driver.find_element_by_id('end_hh'))
+    end_hh.select_by_visible_text('24')
+    driver.find_element_by_id('form_submit').click()
+
+    # switch focus to newly opened tab
+    time.sleep(5)
+    new_tab = driver.window_handles[-1]
+    driver.close()
+    driver.switch_to.window(new_tab)
+
+    # wait for query to process
+    start_time = time.time()
+    result = None
+
+    while ('No matches found.' not in driver.page_source
+           and int(time.time() - start_time) < 240
+           and result is None):
+
+        time.sleep(5)
+        try:
+            result = driver.find_element_by_partial_link_text('.csv')
+        except:
+            pass
+
+    if result is None:
+        return
+
+    all_cookies = driver.get_cookies()
+    cookies = {}
+    for s_cookie in all_cookies:
+        cookies[s_cookie["name"]] = s_cookie["value"]
+
+    url = result.get_attribute('href')
+    response = requests.get(url, cookies=cookies, stream=True, verify=False)
+
+    if not response.ok:
+        print("Bad Response")
+        return
+
+    # download results
+    filename = ticker.replace(" ", "_") + '_' + str(month).zfill(2) + '_' + str(day).zfill(2) + '_' + str(year)[2:]
+    filename = filename + os.path.sep + filename + '_trades.csv'
+    filename = os.path.join(root_dir, 'data', filename)
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, 'wb') as handle:
+        for block in response.iter_content(1024):
+            handle.write(block)
+
+
+# query
+for year in years:
+    for month in months:
+        for day in range(monthrange(year, month)[1]):
+            print("downloading " + str(month) + '/' + str(day+1) + '/'+str(year))
+            for ticker in tickers:
+                download_quotes(ticker, year, month, day+1)
+                download_trades(ticker, year, month, day+1)
 
 time.sleep(2)
 driver.get('https://wrds-web.wharton.upenn.edu/wrds/?logout=true')
