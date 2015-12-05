639e3aa4ff6c45a6599b92cbae47ba1e3bcb969f
Thibaut Xiong <thibaut.xiong@gmail.com>
2015-12-04 22:03:25 -0600

    add feature: add_vpin_time (hit/lift/orderflow) ratio

--- python/datautils/features.py
+++ python/datautils/features.py
@@ -60,14 +60,6 @@ def add_log_return_ema(data, halflives=(10, 40, 100)):
             np.concatenate([[0], (pd.rolling_std(data['log_returns'].values[:-1], 2*hl))])
 
 
-def add_size_diff(data):
-    data['size_diff'] = data['BID_SIZE'] - data['ASK_SIZE']
-
-
-def add_price_diff(data):
-    data['price_diff'] = data['ASK_PRICE'] - data['BID_PRICE']
-
-
 def add_trade_momentum(data, trades, bar_width='second', colname='trade_momentum'):
     minute_bars = (bar_width == 'minute')
     trades = trades.set_index('DATE_TIME')
@@ -98,12 +90,49 @@ def add_trade_momentum_dema(data, trades, halflife=10, bar_width='second', colna
     print "Added {}".format(feat_col_name)
 
 
+def add_size_diff(data):
+    data['size_diff'] = data['BID_SIZE'] - data['ASK_SIZE']
+
+
+def add_price_diff(data):
+    data['price_diff'] = data['ASK_PRICE'] - data['BID_PRICE']
+
+
 def add_mid_price(data):
     data['MID_PRICE'] = data['ASK_PRICE']-data['BID_PRICE']
 
-def add_cum_trade_volume(data):
-    pass
 
+def add_rolling_trade_sum(data, window):
+    start_dates = data['DATE_TIME'] - window
+    data['start_index'] = data['DATE_TIME'].values.searchsorted(start_dates, side='right')
+    data['end_index'] = np.arange(len(data))
+    def sum_window(row):
+        return data['SIZE'].iloc[row['start_index']:row['end_index']+1].sum()
+    data['TRADE_SUM'] = data.apply(sum_window, axis=1)
+    data = data.drop(['start_index', 'end_index'], axis=1, inplace=True)
+
+
+def add_vpin_time(data, window):
+    def lifts(row):
+        lift = -1
+        if row['PRICE'] == row['ASK_PRICE']:
+            lift = 1
+        return row['SIZE']*lift
+    data['lift'] = data.apply(lifts, axis=1)
+    start_dates = data['DATE_TIME'] - window
+    data['start_index'] = data['DATE_TIME'].values.searchsorted(start_dates, side='right')
+    data['end_index'] = np.arange(len(data))
+
+    def sum_window(row):
+        return data['SIZE'].iloc[row['start_index']:row['end_index']+1].sum()
+
+    def sum_lifts(row):
+        return data['lift'].iloc[row['start_index']:row['end_index']+1].sum()
+
+    data['trade_sum'] = data.apply(sum_window, axis=1)
+    data['lift_sum'] = data.apply(sum_lifts, axis=1)
+    data['VPIN_TIME'] = data['lift_sum']/data['trade_sum']
+    data = data.drop(['start_index', 'end_index', 'lift', 'trade_sum', 'lift_sum'], axis=1, inplace=True)
 
 
 class TestFeatures(unittest.TestCase):
